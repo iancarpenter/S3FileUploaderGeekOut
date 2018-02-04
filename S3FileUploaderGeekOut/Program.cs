6d4f402d3da92c778ccda88c4ab4af64c12277c2
Ian Carpenter <ianhcarpenter@gmail.com>
2018-02-04 21:24:31 +0000

    Completed version

--- S3FileUploaderGeekOut/Program.cs
+++ S3FileUploaderGeekOut/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace S3FileUploaderGeekOut
+﻿namespace S3FileUploaderGeekOut
 {
     class Program
     {
         static void Main(string[] args)
         {
+            S3Uploader s3 = new S3Uploader();
+
+            s3.UploadFile();
         }
     }
 }
---
+++ S3FileUploaderGeekOut/S3Uploader.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace S3FileUploaderGeekOut
+{
+
+    /// <summary>
+    /// Based upon https://stackoverflow.com/a/41382560/55640
+    /// </summary>
+    public class S3Uploader
+    {
+        private string bucketName = "YourBucketNameHere";
+        private string keyName = "YourFileNameHere";
+        private string filePath = @"EnterThePathToTheFileAndFileNameHere";
+
+        public void UploadFile()
+        {
+            var client = new AmazonS3Client(Amazon.RegionEndpoint.EUWest2);
+         
+            PutObjectRequest putRequest = new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = keyName,
+                FilePath = filePath,
+                ContentType = "text/plain"
+            };
+
+            PutObjectResponse response = client.PutObject(putRequest);
+            
+        }
+    }
+
+}
